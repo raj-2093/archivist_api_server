559dce1e284f233d94f1a4d84f01b202529e1365
raj-2093 <lunagariyarajy2093@gmail.com>
2024-04-17 14:23:39 +0530

    Added book author field in book schema

--- models/Books.js
+++ models/Books.js
@@ -1,17 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
 const BookSchema = new Schema({
-    BookName:{
-        type: String,
-        required: true
-    },
-    BookId: {
-        type: Number,
-        required: true,
-        unique: true
-    }
+  BookName: {
+    type: String,
+    required: true,
+  },
+  BookId: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  BookAuthor: {
+    type: String,
+    default: null,
+  },
 });
 
-module.exports = mongoose.model('book',BookSchema)
+module.exports = mongoose.model("book", BookSchema);
